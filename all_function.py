3e178019394bc4b2c1e7cc160719dae78b34f163
AlexKomanov <akomanov88@gmail.com>
2025-04-06 11:31:34 +0300

    Add details about abs function

---
+++ all_function.py
@@ -0,0 +1,13 @@
+# num_1: int = 2
+# num_2: float = 2.5
+# num_3: complex = 3 + 4j
+# print(abs(num_1))
+# print(abs(num_2))
+# print(abs(num_3))
+
+num_4: int = -10
+num_5: int = -45.691
+num_6: int = -2 + 3j
+print(abs(num_4))
+print(abs(num_5))
+print(abs(num_6))
