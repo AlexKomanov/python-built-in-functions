4791a8ef78d3f3b2844a07eec03e0ccc644d5c18
AlexKomanov <akomanov88@gmail.com>
2025-04-07 14:39:49 +0300

    adding all function

---
+++ function_02_all.py
@@ -0,0 +1,29 @@
+# iterable_1 = [True, False, True]
+# iterable_2 = [False, False, False]
+# iterable_3 = [True, True, True]
+
+# print(all(iterable_1))
+# print(all(iterable_2))
+# print(all(iterable_3))
+
+# iterable_1 = (1, 0, 1)
+# iterable_2 = (0, 0, 0)
+# iterable_3 = (1, 1, 1)
+
+# print(all(iterable_1))
+# print(all(iterable_2))
+# print(all(iterable_3))
+
+# iterable_1 = ["", " ", " "]
+# iterable_2 = ["", "", ""]
+# iterable_3 = [" ", " ", " "]
+
+# print(all(iterable_1))
+# print(all(iterable_2))
+# print(all(iterable_3))
+
+# Empty iterable returns True
+# print(all([]))
+
+# all() takes exactly one argument (0 given)
+print(all())
