3ce719704d3a821055acc2ea7d5c098f262d1233
AlexKomanov <akomanov88@gmail.com>
2025-04-15 10:35:49 +0300

    Add examples for bin() function

---
+++ function_04_bin.py
@@ -0,0 +1,15 @@
+# print(bin(1000))  
+# Output: 0b1111101000
+
+# print(bin(-100))
+# Output: -0b1100100
+
+# print(bin(0)) 
+# Output: 0b0
+
+# print(bin(1_000_000_020))  
+# Output: 0b111011110110101010101111001000000
+
+# print(bin(2.4))
+
+# print(bin())
