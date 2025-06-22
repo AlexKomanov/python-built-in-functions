85d496d787621c3dc40f880323b7f83dc7dcdb61
AlexKomanov <akomanov88@gmail.com>
2025-06-22 12:44:46 +0300

    Add function_06_char.py demonstrating chr() built-in function

---
+++ function_06_char.py
@@ -0,0 +1,15 @@
+# print(chr(33))
+# print(chr(65))
+# print(chr(69))
+# print(chr(70))
+# print(chr(92))
+# print(chr(97))
+
+# print("a" > "A")  
+
+
+# print(chr(5001))
+
+print(chr(0x00BE))
+print(chr(0x2463))
+print(chr(0x2721))
